9622bd8c93f587cc593d9b14a9b9833c13f957ee
yu3san3 <orange12817@yahoo.co.jp>
2023-05-24 22:28:12 +0900

    CityData.swiftを追加

---
+++ FetchWeatherData/Model/CityData.swift
@@ -0,0 +1,32 @@
+//
+//  CityData.swift
+//  FetchWeatherData
+//
+//  Created by 丹羽雄一朗 on 2023/05/24.
+//
+
+import Foundation
+
+struct CityData: Codable {
+    let location: [Location]
+    
+    enum CodingKeys: String, CodingKey {
+        case location
+    }
+}
+
+struct Location: Codable {
+    let city: String
+    let cityKana: String
+    let town: String
+    let townKana: String
+    let prefecture: String
+    
+    enum CodingKeys: String, CodingKey {
+        case city
+        case cityKana = "city_kana"
+        case town
+        case townKana = "town_kana"
+        case prefecture
+    }
+}
